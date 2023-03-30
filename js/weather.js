a74a41d4b0a66bbbff40afeb743408aad367cd07
常安 <cao11081510@163.com>
2023-03-30 20:22:41 +0800

    Site updated: 2023-03-30 20:22:41

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "10234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "38a4ef009f34448593b85118f9142689"
+  }
+}
